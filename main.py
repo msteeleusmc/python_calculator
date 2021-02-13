1fdae826c09ed9a341a42435dd55e3dd981804d2
msteeleusmc <56514179+msteeleusmc@users.noreply.github.com>
2021-02-12 20:38:22 -0600

    Scientific portion is now built.

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import tkinter.messagebox
 # Set up the object; Object being the calculator
 root = Tk()
 root.title("Scientific Calculator")
-root.configure(background = "gray")
+root.configure(background = "black")
 root.resizable(width = False, height = False)
 root.geometry("480x568+0+0")
 
@@ -59,6 +59,56 @@ btnEquals = Button(calc, text="=", width=6, height=2, font=('arial',20,'bold'), 
                   bg="orange").grid(row=5,column=3,pady=1)
 
 #================================================================================
+# Add scientific functions to the calculator
+btnPi = Button(calc, text="π", width=6, height=2, font=('arial',20,'bold'), bd=4,
+                  bg="tan").grid(row=1,column=4,pady=1)
+btnCos = Button(calc, text="cos", width=6, height=2, font=('arial',20,'bold'), bd=4,
+                  bg="tan").grid(row=1,column=5,pady=1)
+btnTan = Button(calc, text="tan", width=6, height=2, font=('arial',20,'bold'), bd=4,
+                  bg="tan").grid(row=1,column=6,pady=1)
+btnSin = Button(calc, text="sin", width=6, height=2, font=('arial',20,'bold'), bd=4,
+                  bg="tan").grid(row=1,column=7,pady=1)
+
+btn2Pi = Button(calc, text="2π", width=6, height=2, font=('arial',20,'bold'), bd=4,
+                  bg="tan").grid(row=2,column=4,pady=1)
+btnCosh = Button(calc, text="cosh", width=6, height=2, font=('arial',20,'bold'), bd=4,
+                  bg="tan").grid(row=2,column=5,pady=1)
+btnTanh = Button(calc, text="tanh", width=6, height=2, font=('arial',20,'bold'), bd=4,
+                  bg="tan").grid(row=2,column=6,pady=1)
+btnSinh = Button(calc, text="sinh", width=6, height=2, font=('arial',20,'bold'), bd=4,
+                  bg="tan").grid(row=2,column=7,pady=1)
+
+btnLog = Button(calc, text="log", width=6, height=2, font=('arial',20,'bold'), bd=4,
+                  bg="tan").grid(row=3,column=4,pady=1)
+btnExp = Button(calc, text="Exp", width=6, height=2, font=('arial',20,'bold'), bd=4,
+                  bg="tan").grid(row=3,column=5,pady=1)
+btnMod = Button(calc, text="Mod", width=6, height=2, font=('arial',20,'bold'), bd=4,
+                  bg="tan").grid(row=3,column=6,pady=1)
+btnE = Button(calc, text="e", width=6, height=2, font=('arial',20,'bold'), bd=4,
+                  bg="tan").grid(row=3,column=7,pady=1)
+
+btnLog2 = Button(calc, text="log2", width=6, height=2, font=('arial',20,'bold'), bd=4,
+                  bg="tan").grid(row=4,column=4,pady=1)
+btnDeg = Button(calc, text="deg", width=6, height=2, font=('arial',20,'bold'), bd=4,
+                  bg="tan").grid(row=4,column=5,pady=1)
+btnACosh = Button(calc, text="acosh", width=6, height=2, font=('arial',20,'bold'), bd=4,
+                  bg="tan").grid(row=4,column=6,pady=1)
+btnASinh = Button(calc, text="asinh", width=6, height=2, font=('arial',20,'bold'), bd=4,
+                  bg="tan").grid(row=4,column=7,pady=1)
+
+btnLog10 = Button(calc, text="log10", width=6, height=2, font=('arial',20,'bold'), bd=4,
+                  bg="tan").grid(row=5,column=4,pady=1)
+btnLoglp = Button(calc, text="log1p", width=6, height=2, font=('arial',20,'bold'), bd=4,
+                  bg="tan").grid(row=5,column=5,pady=1)
+btnexpml = Button(calc, text="ecpm1", width=6, height=2, font=('arial',20,'bold'), bd=4,
+                  bg="tan").grid(row=5,column=6,pady=1)
+btnlgamma = Button(calc, text="lgamma", width=6, height=2, font=('arial',20,'bold'), bd=4,
+                  bg="tan").grid(row=5,column=7,pady=1)
+
+
+
+
+#================================================================================
 # Exit function allows the calculator to be closed by selecting File->Exit
 def iExit():
     iExit = tkinter.messagebox.askyesno("Scientific Calculator", "Confirm if you want to exit")
@@ -69,7 +119,7 @@ def iExit():
 # Scientific function will change the calculator to a Scientific layout
 def Scientific():
     root.resizable(width=False, height=False)
-    root.geometry("480x568+0+0")
+    root.geometry("958x568+0+0")
 
 # Standard function will change the calculator to a Standard layout
 def Standard():
