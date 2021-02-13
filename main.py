a15bb45ef9013a6513dedf4d9c44728de7717924
msteeleusmc <56514179+msteeleusmc@users.noreply.github.com>
2021-02-12 19:13:08 -0600

    Added a text display portion. Added buttons to the calculator.

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import math
 import parser
 import tkinter.messagebox
 
-
+#================================================================================
 # Set up the object; Object being the calculator
 root = Tk()
 root.title("Scientific Calculator")
@@ -17,6 +17,23 @@ root.geometry("480x568+0+0")
 calc = Frame(root)
 calc.grid()
 
+#================================================================================
+# Creating a text display for the calculator
+textDisplay = Entry(calc, font=('arial',20,'bold'), bg="white",bd=30, width=29, justify=RIGHT)
+textDisplay.grid(row=0, column=0, columnspan=4, pady=1)
+textDisplay.insert(0, "0")
+
+numberpad = "789456123"
+i = 0
+btn = []
+for j in range(2,5):
+    for k in range(3):
+        btn.append(Button(calc, width=6, height=2, font=('arial',20,'bold'), bd=4, text=numberpad[i]))
+        btn[i].grid(row=j, column=k, pady=1)
+        i += 1
+
+
+#================================================================================
 # Exit function allows the calculator to be closed by selecting File->Exit
 def iExit():
     iExit = tkinter.messagebox.askyesno("Scientific Calculator", "Confirm if you want to exit")
@@ -24,18 +41,21 @@ def iExit():
         root.destroy()
         return
 
-# Scientific function will change the calculator to Scientific layout
+# Scientific function will change the calculator to a Scientific layout
 def Scientific():
     root.resizable(width=False, height=False)
     root.geometry("480x568+0+0")
 
-# Standard function will change the calculator to Standard layout
+# Standard function will change the calculator to a Standard layout
 def Standard():
     root.resizable(width=False, height=False)
     root.geometry("480x568+0+0")
 
+#================================================================================
+# Create the menu bar
 menubar = Menu(calc)
 
+#================================================================================
 # Arranging the 'File' menu
 filemenu = Menu(menubar, tearoff = 0)
 # Adds the 'File' option to the window
@@ -49,6 +69,7 @@ filemenu.add_separator()
 # The 'File' menu lets you choose the exit option
 filemenu.add_command(label = "Exit", command = iExit)
 
+#================================================================================
 # Arranging the 'Edit' menu
 editmenu = Menu(menubar, tearoff = 0)
 # Adds the 'Edit' option to the window
@@ -57,18 +78,19 @@ menubar.add_cascade(label = "Edit", menu = editmenu)
 editmenu.add_command(label = "Cut")
 # The 'Edit' menu lets you choose copy
 editmenu.add_command(label = "Copy")
-# Adds seperator below copy
+# Adds separator below copy
 editmenu.add_separator()
 # The 'Edit' menu lets you paste
 editmenu.add_command(label = "Paste")
 
+#================================================================================
 # Create a Help menu
 helpmenu = Menu(menubar, tearoff = 0)
 menubar.add_cascade(label = "Help", menu = helpmenu)
 helpmenu.add_command(label = "View Help")
 
+#================================================================================
 # Print the menu option 'File' to the calculator window
 root.configure(menu = menubar)
-
 # Run a .mainloop() to start the calculator and keep it open
 root.mainloop()
